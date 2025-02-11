b9505eec4afe064bf00b545050cf8f6316160b87
jilei-hao <jilei.cs@gmail.com>
2025-02-11 17:23:26 -0500

    data and exercise

---
+++ src/exercise.py
@@ -0,0 +1,16 @@
+import image
+
+# Excercise 1: Merge specified labels in the input image to the newLabel
+# - Output: A new label image with specified label merged
+def mergeLabel(image, label1, label2, newLabel):
+  return None
+
+
+# Excercise 2: Get a list of unique labels from a image
+def getUniqueLabels(image):
+  return []
+
+
+# Excercise 3: Convert physical sigma mm to vox for the input vtkImage
+def getVoxSigmaFromMm(vtkImage, sigmaInMm):
+  return []
